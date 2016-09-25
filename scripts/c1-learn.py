694c3d888bac6becbee70823847283a858b7ac14
Matt Pietrzykowski <mattpietski@gmail.com>
2016-09-25 00:13:57 -0400

    added a branch off dev for c1 development with a standard folder structure.

---
+++ scripts/c1-learn.py
@@ -0,0 +1,10 @@
+# =============================================================================
+# This python script covers the material in Chapter 1
+#
+# AUTHOR: Matthew Pietrzykowski
+# CREATED: September 25, 2016
+# MODIFIED: 
+#
+# =============================================================================
+
+
